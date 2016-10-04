4709f5a4d0cdeb1509a9b658cb91f4b201c2338c
Alekshar <alekshar.shadow@gmail.com>
2016-10-04 14:30:00 +0200

    first test

--- src/js_basics/index.js
+++ src/js_basics/index.js
@@ -2,30 +2,48 @@
 export const isNumberEven = (i) => {
   // i will be an integer.
   // Return true if it's even, and false if it isn't.
+	return i%2 == 0;
 };
 
 export const getFileExtension = (str) => {
   // str will be a string, but it may not have a file extension.
   // Return the file extension (with no period) if it has one, otherwise false
+	var value = /(?:\.([^.]+))?$/.exec(str)[1];
+	return value; 
 };
 
 export const longestString = (arr) => {
   // arr will be an array.
   // Return the longest string in the array
+	return arr.sort(function (a, b) { return b.length - a.length; })[0];
 };
 
 export const reverseString = (str) => {
   // str will be an string
   // Return a new string who's characters are in the opposite order to str's.
+	return str.split("").reverse().join("");
 };
 
 export const isPalindrome = (str) => {
   // str will be an string
   // Return true if it is a palindrome and false otherwise.
   // It should be case insensitive and not consider space or punctuation.
+	var reversed = reverseString(str);
+	return reversed == str;
 };
 
 export const nestedSum = (arr) => {
   // arr will be an array, containing integers, strings and/or arrays like itself
   // Return the sum all the numbers you find, anywhere in the nest of arrays.
+	var sum = 0;
+	for(var i in arr) {
+		switch (typeof arr[i]) {
+		    case 'array':
+		    	sum+=nestedSum(arr[i]);
+		    	break;
+		    case 'number':
+		    	sum+=arr[i];
+		}
+	}
+	return sum;
 };
